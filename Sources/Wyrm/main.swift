7189aa2274736e346db35083b868927e98cf92b5
Craig Becker <craig@wirefish.com>
2022-06-28 14:10:00 -0700

    print time to load world

--- Sources/Wyrm/main.swift
+++ Sources/Wyrm/main.swift
@@ -9,7 +9,7 @@ let world = World(rootPath: "/Users/craig/Projects/Wyrm/World")
 
 let start = CFAbsoluteTimeGetCurrent()
 world.load()
-print(CFAbsoluteTimeGetCurrent() - start)
+print(String(format: "loaded world in %.4f seconds", CFAbsoluteTimeGetCurrent() - start))
 
 print(world.lookup(EntityRef(module: "isle_of_dawn", name: "officious_kobold"), context: nil)!)
 
