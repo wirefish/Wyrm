9e4ac19afe21f406b47e7963fa88f8022944b75a
Craig Becker <craig@wirefish.com>
2022-07-02 15:28:19 -0700

    factor out general extensions

---
+++ Sources/Wyrm/Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  Extensions.swift
+//  Wyrm
+//
+//  General-purpose extensions of built-in types.
+//
+
+extension Array {
+    // Returns the first non-nil value obtained by applying a transform to the
+    // elements of the array.
+    func firstMap<T>(_ transform: (Element) -> T?) -> T? {
+        for value in self {
+            if let transformedValue = transform(value) {
+                return transformedValue
+            }
+        }
+        return nil
+    }
+}
--- Sources/Wyrm/World.swift
+++ Sources/Wyrm/World.swift
@@ -9,19 +9,6 @@ import CoreFoundation
 import NIOCore
 import NIOPosix
 
-extension Array {
-    // Returns the first non-nil value obtained by applying a transform to the
-    // elements of the array.
-    func firstMap<T>(_ transform: (Element) -> T?) -> T? {
-        for value in self {
-            if let transformedValue = transform(value) {
-                return transformedValue
-            }
-        }
-        return nil
-    }
-}
-
 class Module: ValueDictionary {
     let name: String
     var bindings = [String:Value]()
