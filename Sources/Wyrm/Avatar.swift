a72251bf83098952af2b04735cfa9bc9bb362064
Craig Becker <craig@wirefish.com>
2022-07-04 16:10:47 -0700

    add skills

--- Sources/Wyrm/Avatar.swift
+++ Sources/Wyrm/Avatar.swift
@@ -37,11 +37,14 @@ class Avatar: Entity {
 
     // A mapping from identifiers of completed quests to the time of completion.
     var completedQuests = [ValueRef:CFAbsoluteTime]()
+
+    // Current rank in all known skills.
+    var skills = [ValueRef:Int]()
 }
 
 extension Avatar: Encodable {
     enum CodingKeys: CodingKey {
-        case level, location, equipped, activeQuests
+        case level, location, equipped, activeQuests, completedQuests, skills
     }
 
     func encode(to encoder: Encoder) throws {
@@ -50,5 +53,7 @@ extension Avatar: Encodable {
         try container.encode(location?.ref, forKey: .location)
         try container.encode(equipped, forKey: .equipped)
         // try container.encode(activeQuests, forKey: .activeQuests)
+        try container.encode(completedQuests, forKey: .completedQuests)
+        try container.encode(skills, forKey: .skills)
     }
 }
