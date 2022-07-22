877567295656e8078240f9f80bcb3fddae467108
Craig Becker <craig@wirefish.com>
2022-07-22 07:18:49 -0700

    start on silverwood

--- Sources/Wyrm/main.swift
+++ Sources/Wyrm/main.swift
@@ -9,12 +9,14 @@ let logger = Logger(level: .debug)  // FIXME:
 
 let world = try! World(config: config)
 
+/*
 for i in 2...50 {
     let r = xpRequiredForLevel(i)
     let a = xpAwardedForLevel(i - 1)
     let q = questXPForLevel(i - 1)
     print(i, r, a, Double(r) / Double(a), q, Double(r) / Double(q))
 }
+*/
 
 world.start()
 
