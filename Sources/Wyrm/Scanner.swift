75f341a2c3fc32812e610515f6afdd38416ec4c2
Craig Becker <craig@wirefish.com>
2022-12-09 08:43:16 -0800

    revert indentation and multiline string experiment

--- Sources/Wyrm/Scanner.swift
+++ Sources/Wyrm/Scanner.swift
@@ -4,282 +4,282 @@
 //
 
 enum Token: Hashable {
-  case lparen, rparen, lsquare, rsquare, lbrace, rbrace
-  case colon, comma, dot
+    case lparen, rparen, lsquare, rsquare, lbrace, rbrace
+    case colon, comma, dot
 
-  case minus, minusEqual, plus, plusEqual
-  case slash, slashEqual, star, starEqual
-  case percent, percentEqual
+    case minus, minusEqual, plus, plusEqual
+    case slash, slashEqual, star, starEqual
+    case percent, percentEqual
 
-  case not, notEqual, equal, equalEqual
-  case less, lessEqual, greater, greaterEqual
-  case and, or
+    case not, notEqual, equal, equalEqual
+    case less, lessEqual, greater, greaterEqual
+    case and, or
 
-  case def, deflocation, defquest, defrace, defskill, defregion, extend
-  case allow, before, when, after, phase, `func`
-  case `if`, `else`, `while`, `for`, `in`, `let`, `var`
-  case await, `return`, `fallthrough`
-  case arrow, oneway, to, of
+    case def, deflocation, defquest, defrace, defskill, defregion, extend
+    case allow, before, when, after, phase, `func`
+    case `if`, `else`, `while`, `for`, `in`, `let`, `var`
+    case await, `return`, `fallthrough`
+    case arrow, oneway, to, of
 
-  case `nil`
-  case boolean(Bool)
-  case number(Double)
-  case symbol(String)
-  case string(String)
-  case identifier(String)
+    case `nil`
+    case boolean(Bool)
+    case number(Double)
+    case symbol(String)
+    case string(String)
+    case identifier(String)
 
-  case error(Int, String)
-  case endOfInput
+    case error(Int, String)
+    case endOfInput
 
-  var isAssignment: Bool {
-    return (self == .minusEqual || self == .plusEqual || self == .starEqual ||
-            self == .slashEqual || self == .percentEqual || self == .equal)
-  }
+    var isAssignment: Bool {
+        return (self == .minusEqual || self == .plusEqual || self == .starEqual ||
+                self == .slashEqual || self == .percentEqual || self == .equal)
+    }
 }
 
 extension Character {
-  var isDecimalDigit: Bool {
-    return self >= "0" && self <= "9"
-  }
+    var isDecimalDigit: Bool {
+        return self >= "0" && self <= "9"
+    }
 
-  var isIdentifierChar: Bool {
-    return self.isLetter || self == "_"
-  }
+    var isIdentifierChar: Bool {
+        return self.isLetter || self == "_"
+    }
 }
 
 class Scanner {
-  private let input: String
-  private var index: String.Index
-  private var line = 1
-  private let eof = Character("\0")
+    private let input: String
+    private var index: String.Index
+    private var line = 1
+    private let eof = Character("\0")
 
-  init(_ input: String) {
-    self.input = input
-    index = self.input.startIndex
-  }
+    init(_ input: String) {
+        self.input = input
+        index = self.input.startIndex
+    }
 
-  var currentLine: Int { line }
+    var currentLine: Int { line }
 
-  func nextToken() -> Token {
-    skipWhitespace()
-    let ch = peek()
-    if ch.isDecimalDigit {
-      return scanNumber()
-    } else if ch == "\"" {
-      return scanString()
-    } else if ch.isLetter {
-      return scanIdentifier()
-    } else {
-      advance()
-      switch ch {
-      case eof: return .endOfInput
-      case "(": return .lparen
-      case ")": return .rparen
-      case "[": return .lsquare
-      case "]": return .rsquare
-      case "{": return .lbrace
-      case "}": return .rbrace
-      case ":": return .colon
-      case ",": return .comma
-      case ".": return .dot
-      case "'": return scanSymbol()
-      case "-": return match("=") ? .minusEqual : match(">") ? .arrow : .minus
-      case "+": return match("=") ? .plusEqual : .plus
-      case "*": return match("=") ? .starEqual : .star
-      case "%": return match("=") ? .percentEqual : .percent
-      case "!": return match("=") ? .notEqual : .not
-      case "<": return match("=") ? .lessEqual : .less
-      case ">": return match("=") ? .greaterEqual : .greater
-      case "=": return match("=") ? .equalEqual : .equal
-
-      case "/":
-        if peek() == "*" || peek() == "/" {
-          skipComment()
-          return nextToken()
+    func nextToken() -> Token {
+        skipWhitespace()
+        let ch = peek()
+        if ch.isDecimalDigit {
+            return scanNumber()
+        } else if ch == "\"" {
+            return scanString()
+        } else if ch.isLetter {
+            return scanIdentifier()
         } else {
-          return match("=") ? .slashEqual : .slash
+            advance()
+            switch ch {
+            case eof: return .endOfInput
+            case "(": return .lparen
+            case ")": return .rparen
+            case "[": return .lsquare
+            case "]": return .rsquare
+            case "{": return .lbrace
+            case "}": return .rbrace
+            case ":": return .colon
+            case ",": return .comma
+            case ".": return .dot
+            case "'": return scanSymbol()
+            case "-": return match("=") ? .minusEqual : match(">") ? .arrow : .minus
+            case "+": return match("=") ? .plusEqual : .plus
+            case "*": return match("=") ? .starEqual : .star
+            case "%": return match("=") ? .percentEqual : .percent
+            case "!": return match("=") ? .notEqual : .not
+            case "<": return match("=") ? .lessEqual : .less
+            case ">": return match("=") ? .greaterEqual : .greater
+            case "=": return match("=") ? .equalEqual : .equal
+
+            case "/":
+                if peek() == "*" || peek() == "/" {
+                    skipComment()
+                    return nextToken()
+                } else {
+                    return match("=") ? .slashEqual : .slash
+                }
+
+            case "&": return match("&") ? .and : .error(line, "unexpected character after &")
+
+            case "|":
+                if match("|") {
+                    return .or
+                } else if peek().isNewline {
+                    advance()
+                    return scanText()
+                } else {
+                    return .error(line, "unexpected character after |")
+                }
+
+            default:
+                return .error(line, "unexpected character at \(ch)")
+            }
         }
+    }
 
-      case "&": return match("&") ? .and : .error(line, "unexpected character after &")
+    private func peek() -> Character {
+        return index < input.endIndex ? input[index] : eof
+    }
 
-      case "|":
-        if match("|") {
-          return .or
-        } else if peek().isNewline {
-          advance()
-          return scanText()
+    @discardableResult
+    private func advance() -> Character {
+        let ch = peek()
+        if ch != eof {
+            index = input.index(after: index)
+            if ch.isNewline {
+                line += 1
+            }
+        }
+        return ch
+    }
+
+    private func match(_ ch: Character) -> Bool {
+        if peek() == ch {
+            advance()
+            return true
         } else {
-          return .error(line, "unexpected character after |")
+            return false
         }
+    }
 
-      default:
-        return .error(line, "unexpected character at \(ch)")
-      }
+    private func skipWhitespace() {
+        while peek().isWhitespace {
+            advance()
+        }
     }
-  }
 
-  private func peek() -> Character {
-    return index < input.endIndex ? input[index] : eof
-  }
+    private func skipComment() {
+        if match("*") {
+            // Skip until the next "*/".
+            while advance() != "*" || !match("/") {}
+        } else if match("/") {
+            // Skip until the end of this line.
+            while !advance().isNewline {}
+        }
+    }
 
-  @discardableResult
-  private func advance() -> Character {
-    let ch = peek()
-    if ch != eof {
-      index = input.index(after: index)
-      if ch.isNewline {
-        line += 1
-      }
+    private func scanNumber() -> Token {
+        let start = index
+        while peek().isDecimalDigit {
+            advance()
+        }
+        if match(".") {
+            while peek().isDecimalDigit {
+                advance()
+            }
+        }
+        return .number(Double(input[start..<index])!)
     }
-    return ch
-  }
 
-  private func match(_ ch: Character) -> Bool {
-    if peek() == ch {
-      advance()
-      return true
-    } else {
-      return false
+    private func scanString() -> Token {
+        advance()  // past leading double quote
+        let start = index
+        while index < input.endIndex && peek() != "\"" {
+            let ch = advance()
+            if ch == "\\" {
+                advance()
+            }
+        }
+        if index == input.endIndex {
+            return .error(line, "unterminated string literal")
+        } else {
+            let token = Token.string(String(input[start..<index]))
+            advance()  // past ending double quote
+            return token
+        }
     }
-  }
 
-  private func skipWhitespace() {
-    while peek().isWhitespace {
-      advance()
+    private func scanIndent() -> Int {
+        var indent = 0
+        while peek().isWhitespace && !peek().isNewline {
+            advance()
+            indent += 1
+        }
+        return indent
     }
-  }
 
-  private func skipComment() {
-    if match("*") {
-      // Skip until the next "*/".
-      while advance() != "*" || !match("/") {}
-    } else if match("/") {
-      // Skip until the end of this line.
-      while !advance().isNewline {}
+    private func scanText() -> Token {
+        let baseIndent = scanIndent()
+        var result = ""
+        while index < input.endIndex {
+            let start = index
+            while !advance().isNewline { }
+            result.append(contentsOf: input[start..<index])
+            let indent = scanIndent()
+            if peek().isNewline {
+            } else if indent < baseIndent {
+                break
+            } else if indent > baseIndent {
+                result.append(contentsOf: String(repeating: " ", count: indent - baseIndent))
+            }
+        }
+        while result.hasSuffix("\n\n") {
+            result.removeLast()
+        }
+        if !result.contains("\n\n") {
+            result.removeLast()
+        }
+        return .string(result)
     }
-  }
 
-  private func scanNumber() -> Token {
-    let start = index
-    while peek().isDecimalDigit {
-      advance()
-    }
-    if match(".") {
-      while peek().isDecimalDigit {
-        advance()
-      }
-    }
-    return .number(Double(input[start..<index])!)
-  }
+    private let keywords: [String:Token] = [
+        "nil": .nil,
+        "true": .boolean(true),
+        "false": .boolean(false),
+        "def": .def,
+        "deflocation": .deflocation,
+        "defquest": .defquest,
+        "defrace": .defrace,
+        "defskill": .defskill,
+        "defregion": .defregion,
+        "extend": .extend,
+        "allow": .allow,
+        "before": .before,
+        "when": .when,
+        "after": .after,
+        "phase": .phase,
+        "func": .func,
+        "if": .if,
+        "else": .else,
+        "while": .while,
+        "for": .for,
+        "in": .in,
+        "let": .let,
+        "var": .var,
+        "await": .await,
+        "return": .return,
+        "fallthrough": .fallthrough,
+        "oneway": .oneway,
+        "to": .to,
+        "of": .of,
+    ]
 
-  private func scanString() -> Token {
-    advance()  // past leading double quote
-    let start = index
-    while index < input.endIndex && peek() != "\"" {
-      let ch = advance()
-      if ch == "\\" {
-        advance()
-      }
+    private func consumeIdentifier() -> String? {
+        let start = index
+        if peek().isIdentifierChar {
+            advance()
+            while peek().isIdentifierChar || peek().isNumber {
+                advance()
+            }
+            return String(input[start..<index])
+        } else {
+            return nil
+        }
     }
-    if index == input.endIndex {
-      return .error(line, "unterminated string literal")
-    } else {
-      let token = Token.string(String(input[start..<index]))
-      advance()  // past ending double quote
-      return token
-    }
-  }
 
-  private func scanIndent() -> Int {
-    var indent = 0
-    while peek().isWhitespace && !peek().isNewline {
-      advance()
-      indent += 1
+    private func scanIdentifier() -> Token {
+        guard let id = consumeIdentifier() else {
+            return .error(line, "invalid identifier")
+        }
+        return keywords[id] ?? .identifier(id)
     }
-    return indent
-  }
 
-  private func scanText() -> Token {
-    let baseIndent = scanIndent()
-    var result = ""
-    while index < input.endIndex {
-      let start = index
-      while !advance().isNewline { }
-      result.append(contentsOf: input[start..<index])
-      let indent = scanIndent()
-      if peek().isNewline {
-      } else if indent < baseIndent {
-        break
-      } else if indent > baseIndent {
-        result.append(contentsOf: String(repeating: " ", count: indent - baseIndent))
-      }
+    private func scanSymbol() -> Token {
+        // Note the leading ' has already been consumed.
+        guard let id = consumeIdentifier() else {
+            return .error(line, "invalid symbolic constant")
+        }
+        return .symbol(id)
     }
-    while result.hasSuffix("\n\n") {
-      result.removeLast()
-    }
-    if !result.contains("\n\n") {
-      result.removeLast()
-    }
-    return .string(result)
-  }
-
-  private let keywords: [String:Token] = [
-    "nil": .nil,
-    "true": .boolean(true),
-    "false": .boolean(false),
-    "def": .def,
-    "deflocation": .deflocation,
-    "defquest": .defquest,
-    "defrace": .defrace,
-    "defskill": .defskill,
-    "defregion": .defregion,
-    "extend": .extend,
-    "allow": .allow,
-    "before": .before,
-    "when": .when,
-    "after": .after,
-    "phase": .phase,
-    "func": .func,
-    "if": .if,
-    "else": .else,
-    "while": .while,
-    "for": .for,
-    "in": .in,
-    "let": .let,
-    "var": .var,
-    "await": .await,
-    "return": .return,
-    "fallthrough": .fallthrough,
-    "oneway": .oneway,
-    "to": .to,
-    "of": .of,
-  ]
-
-  private func consumeIdentifier() -> String? {
-    let start = index
-    if peek().isIdentifierChar {
-      advance()
-      while peek().isIdentifierChar || peek().isNumber {
-        advance()
-      }
-      return String(input[start..<index])
-    } else {
-      return nil
-    }
-  }
-
-  private func scanIdentifier() -> Token {
-    guard let id = consumeIdentifier() else {
-      return .error(line, "invalid identifier")
-    }
-    return keywords[id] ?? .identifier(id)
-  }
-
-  private func scanSymbol() -> Token {
-    // Note the leading ' has already been consumed.
-    guard let id = consumeIdentifier() else {
-      return .error(line, "invalid symbolic constant")
-    }
-    return .symbol(id)
-  }
 }
