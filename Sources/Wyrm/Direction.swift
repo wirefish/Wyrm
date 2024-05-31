61fe8f3b8b8f10e4634ff15434ce72146dba3693
Craig Becker <craig@wirefish.com>
2024-05-31 07:29:46 -0700

    reindent

--- Sources/Wyrm/Direction.swift
+++ Sources/Wyrm/Direction.swift
@@ -3,62 +3,60 @@
 //  Wyrm
 //
 
-import CoreFoundation
-
 enum Direction: Int, ValueRepresentableEnum {
-    case north = 0, northeast, east, southeast, south, southwest, west, northwest
-    case up, down, `in`, out
+  case north = 0, northeast, east, southeast, south, southwest, west, northwest
+  case up, down, `in`, out
 
-    var opposite: Direction {
-        switch self {
-        case .north: return .south
-        case .northeast: return .southwest
-        case .east: return .west
-        case .southeast: return .northwest
-        case .south: return .north
-        case .southwest: return .northeast
-        case .west: return .east
-        case .northwest: return .southeast
-        case .up: return .down
-        case .down: return .up
-        case .in: return .out
-        case .out: return .in
-        }
+  var opposite: Direction {
+    switch self {
+    case .north: return .south
+    case .northeast: return .southwest
+    case .east: return .west
+    case .southeast: return .northwest
+    case .south: return .north
+    case .southwest: return .northeast
+    case .west: return .east
+    case .northwest: return .southeast
+    case .up: return .down
+    case .down: return .up
+    case .in: return .out
+    case .out: return .in
     }
+  }
 
-    var offset: (Int, Int, Int) {
-        switch self {
-        case .north: return (0, -1, 0)
-        case .northeast: return (1, -1, 0)
-        case .east: return (1, 0, 0)
-        case .southeast: return (1, 1, 0)
-        case .south: return (0, 1, 0)
-        case .southwest: return (-1, 1, 0)
-        case .west: return (-1, 0, 0)
-        case .northwest: return (-1, -1, 0)
-        case .up: return (0, 0, 1)
-        case .down: return (0, 0, -1)
-        case .in: return (0, 0, 0)
-        case .out: return (0, 0, 0)
-        }
+  var offset: (Int, Int, Int) {
+    switch self {
+    case .north: return (0, -1, 0)
+    case .northeast: return (1, -1, 0)
+    case .east: return (1, 0, 0)
+    case .southeast: return (1, 1, 0)
+    case .south: return (0, 1, 0)
+    case .southwest: return (-1, 1, 0)
+    case .west: return (-1, 0, 0)
+    case .northwest: return (-1, -1, 0)
+    case .up: return (0, 0, 1)
+    case .down: return (0, 0, -1)
+    case .in: return (0, 0, 0)
+    case .out: return (0, 0, 0)
     }
+  }
 
-    static let names = Dictionary(uniqueKeysWithValues: Self.allCases.map {
-        (String(describing: $0), $0)
-    })
+  static let names = Dictionary(uniqueKeysWithValues: Self.allCases.map {
+    (String(describing: $0), $0)
+  })
 }
 
 extension Direction: Matchable {
-    // FIXME: allow abbreviations like 'sw' for 'southwest'.
-    func match(_ tokens: ArraySlice<String>) -> MatchQuality {
-        if tokens.count == 1 {
-            let name = String(describing: self)
-            if tokens[0] == name {
-                return .exact
-            } else if name.hasPrefix(tokens[0]) {
-                return .partial
-            }
-        }
-        return .none
+  // FIXME: allow abbreviations like 'sw' for 'southwest'.
+  func match(_ tokens: ArraySlice<String>) -> MatchQuality {
+    if tokens.count == 1 {
+      let name = String(describing: self)
+      if tokens[0] == name {
+        return .exact
+      } else if name.hasPrefix(tokens[0]) {
+        return .partial
+      }
     }
+    return .none
+  }
 }
