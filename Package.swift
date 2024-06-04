8d1aa2e0d0b6a0d4dbe0e37b622dc7cb94d28b81
Craig Becker <craig@wirefish.com>
2024-06-04 14:49:57 -0700

    reindent

--- Package.swift
+++ Package.swift
@@ -5,29 +5,29 @@ import PackageDescription
 import CompilerPluginSupport
 
 let package = Package(
-    name: "Wyrm",
-    platforms: [.macOS(.v14)],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/dduan/TOMLDecoder", from: "0.2.2"),
-        .package(url: "https://github.com/apple/swift-syntax", from: "509.0.0")
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can
-        // define a module or a test suite. Targets can depend on other targets
-        // in this package, and on products in packages this package depends on.
-        .executableTarget(
-            name: "Wyrm",
-            dependencies: ["TOMLDecoder", "WyrmMacros"]),
-        .testTarget(
-            name: "WyrmTests",
-            dependencies: ["Wyrm"]),
-        .macro(
-            name: "WyrmMacros",
-            dependencies: [
-                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
-            ]),
-    ]
+  name: "Wyrm",
+  platforms: [.macOS(.v14)],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+    .package(url: "https://github.com/dduan/TOMLDecoder", from: "0.2.2"),
+    .package(url: "https://github.com/apple/swift-syntax", from: "509.0.0")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can
+    // define a module or a test suite. Targets can depend on other targets
+    // in this package, and on products in packages this package depends on.
+    .executableTarget(
+      name: "Wyrm",
+      dependencies: ["TOMLDecoder", "WyrmMacros"]),
+    .testTarget(
+      name: "WyrmTests",
+      dependencies: ["Wyrm"]),
+    .macro(
+      name: "WyrmMacros",
+      dependencies: [
+        .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+        .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+      ]),
+  ]
 )
