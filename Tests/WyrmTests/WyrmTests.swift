e14097a8ab9bc51cae2b1971258ae2a3f4655201
Craig Becker <craig@wirefish.com>
2024-06-05 11:55:52 -0700

    add some NounPhrase tests

--- Tests/WyrmTests/WyrmTests.swift
+++ Tests/WyrmTests/WyrmTests.swift
@@ -1,6 +1,23 @@
 import XCTest
+@testable import Wyrm
 
-final class WyrmTests: XCTestCase {
-    func testExample() throws {
+final class NounPhraseTests: XCTestCase {
+  static let examples = [
+    ("a rock", "a", "rock", "rocks"),
+    ("the fox", "the", "fox", "foxes"),
+    ("apple", "an", "apple", "apples"),
+    ("box[es] of dirt", "a", "box of dirt", "boxes of dirt"),
+    ("sarcophag[us|i]", "a", "sarcophagus", "sarcophagi"),
+    ("t[oo|ee]th", "a", "tooth", "teeth"),
+    ("Bob", nil, "Bob", "Bob")
+  ]
+
+  func testInit() throws {
+    for (phrase, article, singular, plural) in Self.examples {
+      let noun = NounPhrase(phrase)
+      XCTAssertEqual(noun.article, article)
+      XCTAssertEqual(noun.singular, singular)
+      XCTAssertEqual(noun.plural, plural)
     }
+  }
 }
